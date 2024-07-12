02d00a91a44550e31105412e44c9edd0f9b39751
joel-sitte <joel.sitte@gmail.com>
2024-07-12 14:10:26 -0700

    Initial success for magnify and drag gestures coexisting

--- Sources/GodotVision+SwiftUI.swift
+++ Sources/GodotVision+SwiftUI.swift
@@ -6,27 +6,53 @@ import RealityKit
 
 public struct GodotVisionRealityViewModifier: ViewModifier {
     var coordinator: GodotVisionCoordinator
+    
+    //@GestureState private var zoomFactor: CGFloat = 1.0
 
     public init(coordinator: GodotVisionCoordinator) {
         self.coordinator = coordinator
     }
 
     public func body(content: Content) -> some View {
+        let tapGesture = SpatialTapGesture().targetedToAnyEntity().onEnded { event in
+            if event.entity.components.has(InputTargetComponent.self) {
+                coordinator.receivedTap(event: event)
+            }
+        }
+        
+        let dragGesture = DragGesture(minimumDistance: 0.1, coordinateSpace: .local).targetedToAnyEntity().onChanged({ value in
+            if value.entity.components.has(InputTargetComponent.self) {
+                coordinator.receivedDrag(value)
+            }
+        }).onEnded({value in
+            if value.entity.components.has(InputTargetComponent.self) {
+                coordinator.receivedDragEnded(value)
+            }
+        })
+        
+        let magnifyGesture = MagnifyGesture(minimumScaleDelta: 0.01).targetedToAnyEntity()
+        //.updating($zoomFactor, body: {value, scale, transaction in
+        //    if value.entity.components.has(InputTargetComponent.self) {
+        //        coordinator.receivedMagnify(value)
+        //    }
+        //})
+        .onChanged({ value in
+            if value.entity.components.has(InputTargetComponent.self) {
+                coordinator.receivedMagnify(value)
+            }
+        })
+        .onEnded({value in
+            if value.entity.components.has(InputTargetComponent.self) {
+                coordinator.receivedMagnifyEnded(value)
+            }
+        })
+        
         content
-            .gesture(SpatialTapGesture().targetedToAnyEntity().onEnded { event in
-                if event.entity.components.has(InputTargetComponent.self) {
-                    coordinator.receivedTap(event: event)
-                }
-            })
-            .gesture(DragGesture(minimumDistance: 0.1, coordinateSpace: .local).targetedToAnyEntity().onChanged({ value in
-                if value.entity.components.has(InputTargetComponent.self) {
-                    coordinator.receivedDrag(value)
-                }
-            }).onEnded({value in
-                if value.entity.components.has(InputTargetComponent.self) {
-                    coordinator.receivedDragEnded(value)
-                }
-            }))
+            .gesture(tapGesture)
+            //.gesture(dragGesture)
+            //.gesture(magnifyGesture)
+            //.gesture(magnifyGesture.exclusively(before: dragGesture))
+            .gesture(magnifyGesture.simultaneously(with: dragGesture))
             .onDisappear {
                 coordinator.viewDidDisappear()
             }
